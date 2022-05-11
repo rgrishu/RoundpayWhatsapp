75ba062bdefa7e3ec58345284fbea293476c0306
rgrishu <rishabhgoswami143@gmail.com>
2022-05-11 10:44:51 +0530

    h

--- Whatsapp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Whatsapp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -119,7 +119,7 @@ namespace Whatsapp.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
+         
             return Page();
         }
     }
