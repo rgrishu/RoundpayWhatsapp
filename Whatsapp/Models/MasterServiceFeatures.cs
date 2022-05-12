47f36924f895404229eb7c1bcbb681d310cba989
Roundpay Developer Akash <Roundpay Developer@DESKTOP-3FMP3QI>
2022-05-12 17:40:09 +0530

    Added foreign key relation for Service and feature

--- Whatsapp/Models/MasterServiceFeatures.cs
+++ Whatsapp/Models/MasterServiceFeatures.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,14 +16,13 @@ namespace Whatsapp.Models
             get;
             set;
         }
-        public Int64 ServiceID
-        {
-            get;
-            set;
-        }
         public string FeatureName { get; set; }
         public string Remark { get; set; }
         public bool IsActive { get; set; }
-      
+        public Int64 ServiceID { get; set; }
+
+        [ForeignKey("ServiceID")]
+        public MasterService MasterService { get; set; }
+
     }
 }
