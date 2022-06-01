e48d2fd3dcc4d3445bca2a23a5432019d5fa4d57
Roundpay Developer Akash <Roundpay Developer@DESKTOP-3FMP3QI>
2022-06-01 14:41:17 +0530

    Add Fund for Admin And UI Changes based on Role

---
+++ Whatsapp/Migrations/20220601075935_AddedForeignRelationsklk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Whatsapp.Migrations
+{
+    public partial class AddedForeignRelationsklk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TransactionType",
+                table: "Ledgers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LockoutEnd",
+                value: new DateTimeOffset(new DateTime(2022, 6, 1, 13, 29, 35, 258, DateTimeKind.Unspecified).AddTicks(6044), new TimeSpan(0, 5, 30, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TransactionType",
+                table: "Ledgers",
+                type: "nvarchar(1)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LockoutEnd",
+                value: new DateTimeOffset(new DateTime(2022, 6, 1, 11, 2, 0, 767, DateTimeKind.Unspecified).AddTicks(3594), new TimeSpan(0, 5, 30, 0, 0)));
+        }
+    }
+}
