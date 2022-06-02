3066765a81fc84e97987a18c9b320c633040fc3c
Roundpay Developer Akash <Roundpay Developer@DESKTOP-3FMP3QI>
2022-06-02 19:00:40 +0530

    Message Header UI

---
+++ Whatsapp/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Whatsapp.Controllers
+{
+    [Authorize]
+    public class MessageController : Controller
+    {
+        [Route("MessageTemplate")]
+        public IActionResult MessageTemplate()
+        {
+            return View();
+        }
+    }
+}
